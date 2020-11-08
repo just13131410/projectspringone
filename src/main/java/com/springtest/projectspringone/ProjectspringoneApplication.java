3bcf3c6afd644c18775bb3df220c61b3b462bd72
justme <just-k@gmx.net>
2020-11-08 16:43:54 +0100

    first commit

---
+++ src/main/java/com/springtest/projectspringone/ProjectspringoneApplication.java
@@ -0,0 +1,13 @@
+package com.springtest.projectspringone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectspringoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectspringoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springtest/projectspringone/ProjectspringoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springtest.projectspringone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectspringoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
