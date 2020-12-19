c2072a293f894fd408190131edc99fd7ca67dd7e
justme <just-k@gmx.net>
2020-12-19 14:13:13 +0100

    added 1st unittest

--- src/test/java/net/javaguides/springboot/SpringbootBackendApplicationTests.java
+++ src/test/java/net/javaguides/springboot/SpringbootBackendApplicationTests.java
@@ -1,13 +1,23 @@
 package net.javaguides.springboot;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import net.javaguides.springboot.controller.EmployeeController;
 
 @SpringBootTest
 class SpringbootBackendApplicationTests {
 
+	@Autowired
+	private EmployeeController controller;
+
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
 
 }
